2a576c88d6266b1b7ab44ae29dc3bae73d425ae6
Ran Golani <rango2001@gmail.com>
2024-11-10 09:42:58 +0200

    better homework

--- 2024-11-05/homework/javascript-conditions-loops-continue/12/assets/js/app.js
+++ 2024-11-05/homework/javascript-conditions-loops-continue/12/assets/js/app.js
@@ -4,8 +4,15 @@ for(let i=1; i<=num; i+=2) {
     document.write(`${z}&nbsp`);};
     document.write(`<br>`);
 };
+if (num%2===1){
 for(let i=num-2; i>=1; i-=2) {
   for(let z=1; z<=i; z++){
     document.write(`${z}&nbsp`);};
     document.write(`<br>`);
-};
+};}
+else {
+  for(let i=num-3; i>=1; i-=2) {
+    for(let z=1; z<=i; z++){
+      document.write(`${z}&nbsp`);};
+      document.write(`<br>`);
+};};
--- 2024-11-05/homework/javascript-conditions-loops-continue/4/assets/js/app.js
+++ 2024-11-05/homework/javascript-conditions-loops-continue/4/assets/js/app.js
@@ -1,6 +1,10 @@
 let num = +prompt('please enter a number');
+while (num > 0) {
 for(let i=num; i>=2; i--) {
   if(i%2===0) {
-  document.write(`${i}&nbsp`);
+  // document.write(`${i}&nbsp`);
+  console.log(i);
   } else { };
+};
+num = +prompt('please enter a number');
 };
